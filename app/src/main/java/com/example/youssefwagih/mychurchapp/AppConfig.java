0097cb0840b6800396be3eb42ecd44df1931e90d
DESKTOP-O7KG27O\Youssef Wagih <youssefwagih93@gmail.com>
2016-11-26 12:23:15 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/youssefwagih/mychurchapp/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.youssefwagih.mychurchapp;
+
+/**
+ * Created by Youssef Wagih on 11/17/2016.
+ */
+
+public class AppConfig {
+    public static final String PARSE_CHANNEL = "AndroidHive";
+    public static final String PARSE_APPLICATION_ID = "ZUSZNPfsop5GnsjZPc4ajRittIUj3ilvC8dPLbC0";
+    public static final String PARSE_CLIENT_KEY = "3kCVPLxA09hFUCkWB9Hc5isS6lOb6iYtG2gIcf1k";
+    public static final int NOTIFICATION_ID = 100;
+}
